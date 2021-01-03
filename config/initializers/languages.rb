dd89ed8ab8cf137c1a5aad9d2ac5faab044fe076
Nguyễn Đức Hiếu <nguyenduchieu1998@gmail.com>
2021-01-03 23:36:04 +0700

    Update languages.rb

--- config/initializers/languages.rb
+++ config/initializers/languages.rb
@@ -15,5 +15,5 @@ Rails.configuration.i18n.available_locales = configured_languages
 # the I18n.default_locale when a translation cannot be found).
 Rails.configuration.i18n.fallbacks = {}
 Rails.configuration.i18n.available_locales.each do |locale|
-  Rails.configuration.i18n.fallbacks[locale] = :vi_VN
+  Rails.configuration.i18n.fallbacks[locale] = :en
 end
